8f11fe28666a72ed6996cbedba39b4a7894fbf44
Arnab <arnabmukhopadhyay76@gmail.com>
2022-01-14 01:15:53 +0530

    data generate fixed

--- __main__.py
+++ __main__.py
@@ -1,6 +1,12 @@
+from datagen import data
+
 
 def main():
     print("Hello World")
+    test = data.gen_data(2, 10, 20, 2, 4)
+    print("The final data generated is: ")
+    print(test)
+
 
 if __name__ == '__main__':
     main()
